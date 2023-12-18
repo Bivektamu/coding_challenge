7898e9ac8a8c1ff28d7199ba8dc37db9e9cef7c7
Bivek <bivek.tamu@gmail.com>
2023-12-18 23:20:04 +1100

    Sortable list challenge

---
+++ src/component/SortableList/Skill.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Skill = () => {
+  return (
+    <div className="FaqOne w-[392px] p-4 bg-blue-950 rounded-lg border-l-4 justify-between items-center inline-flex">
+      <div className="Javascript text-center text-white text-lg font-medium font-['Poppins']">JavaScript</div>
+      <div className="Plus w-5 h-5 relative origin-top-left -rotate-45" />
+    </div>
+  )
+}
+
+export default Skill
