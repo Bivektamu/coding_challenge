45780800a25104ddedd9c86c7ea11fa998165c46
Bivek <bivek.tamu@gmail.com>
2023-12-19 14:42:50 +1100

    Search and suggesting skills feature added.

--- src/component/SortableList/Skill.jsx
+++ src/component/SortableList/Skill.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Skill = () => {
+const Skill = ({skill, index, onClickHandler}) => {
+
+
   return (
-    <div className="FaqOne w-[392px] p-4 bg-blue-950 rounded-lg border-l-4 justify-between items-center inline-flex">
-      <div className="Javascript text-center text-white text-lg font-medium font-['Poppins']">JavaScript</div>
-      <div className="Plus w-5 h-5 relative origin-top-left -rotate-45" />
+    <div className="FaqFour w-[392px] h-[59px] p-4  rounded-lg border border-slate-200 justify-between items-center inline-flex relative bg-blue-950">
+      <div  className="AddSkill text-white outline-none bg-transparent  text-lg font-normal font-['Poppins']">{index+1}. {skill}</div>
+      <span className="Plus w-5 h-5 absolute text-white top-2 right-4 text-2xl cursor-pointer" onClick={() => onClickHandler(index)}>x</span>
     </div>
   )
 }
--- src/component/SortableList/SkillsBoard.jsx
+++ src/component/SortableList/SkillsBoard.jsx
@@ -3,45 +3,70 @@ import Skill from './Skill'
 import SKILLS from '../../dataLayer'
 
 const SkillsBoard = () => {
+    const [selectedSkills, setSs] = useState([])
     const [value, setV] = useState('')
-    const [sugg, setS] = useState([])
+    const [suggList, setSL] = useState([])
+    const [suggest, setS] = useState(false)
+    const [optionSelected, setOS] = useState(false)
+    const [allS, setAS] = useState(SKILLS)
+
+
     useEffect(() => {
+
         if (value) {
             console.clear()
-            const allS = SKILLS
             // const filtered = allS.filter(s=> s.indexOf(value) === 0)
-
             const filtered = allS.filter(s => {
                 const index = (s.toLocaleLowerCase().indexOf(value.toLocaleLowerCase()))
                 if (index === 0) {
-                    console.log(s)
-                    return s
+                    return (s)
                 }
             })
-            setS(filtered)
+            setSL(filtered)
         }
 
     }, [value])
+
+    const selected = (s) => {
+        setSs([...selectedSkills, s])
+        setOS(true)
+        setV('')
+        setSL([])
+        setS(false)
+        let temp = allS.filter(sk=>sk!==s)
+        setAS(temp)
+    }
+
+    const deleteSkill = (i) => {
+        console.log(i)
+        const temp = selectedSkills.filter((s,index) => index!== i)
+        console.log(temp)
+        setSs(temp)
+
+    }
+
     return (
         <div className='board'>
             <div className="Frame1 w-[717px] h-[498.85px] p-16 bg-white rounded-2xl shadow justify-start items-start gap-16 inline-flex">
                 <div className="Skills flex-col justify-center items-center gap-[18px] inline-flex">
-                    {/* <Skill /> */}
-                    <div className="FaqFour w-[392px] h-[59px] p-4 bg-gray-50 rounded-lg border border-slate-200 justify-between items-center inline-flex relative">
-                        <input className="AddSkill text-slate-500 text-lg font-normal font-['Poppins']" placeholder='Add Skill' value={value} onChange={e => setV(e.target.value)} />
+                    {selectedSkills.length > 0 && selectedSkills.map((s, i) => <Skill key={i} skill={s} index={i} onClickHandler={()=>deleteSkill(i)} />)}
+                    {selectedSkills.length < 5 && (
+                        <div className={`FaqFour w-[392px] h-[59px] p-4  rounded-lg border border-slate-200 justify-between items-center inline-flex relative bg-gray-50`}>
+                            <input className={`AddSkill text-slate-500 outline-none bg-transparent  text-lg font-normal font-['Poppins'] `} placeholder={`${selectedSkills.length+1}. Add Skill`} value={value} onChange={e => setV(e.target.value)} onFocus={() => setS(true)} />
 
-                        {sugg.length > 0 && (
-                            <div className="FaqFour absolute left-0 top-14 w-[392px]  bg-gray-50 rounded-lg border border-slate-200">
-                                {sugg.map(s => (
-                                    <div onClick={()=>setV(s)} className="Rectangle1 hover:bg-slate-200 cursor-pointer border-l border-r border-slate-200">
-                                        <p className="Reactjs text-slate-500 text-base font-normal font-['Poppins'] text-left p-2">{s}</p>
-                                    </div>
-                                ))}
-                            </div>
-                        )}
+                            {suggest && suggList.length > 0 && (
+                                <div className="FaqFour absolute left-0 top-14 w-[392px] max-h-[250px] overflow-auto z-10  bg-gray-50 rounded-lg border border-slate-200">
+                                    {suggList.map((s, i) => (
+                                        <div key={i} className="Rectangle1 hover:bg-slate-200 cursor-pointer border-l border-r border-slate-200">
+                                            <p className="Reactjs text-slate-500 text-base font-normal font-['Poppins'] text-left p-2" onClick={() => selected(s)}>{s}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
 
 
-                    </div>
+                        </div>
+                    )}
                 </div>
                 <div className="SuggestedSkills w-[133px] h-[234px] relative">
                     <div className="Frame1 left-0 top-[40px] absolute flex-col justify-start items-start gap-1.5 inline-flex">
