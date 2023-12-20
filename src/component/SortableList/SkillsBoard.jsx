284a36de948bcf96cc1b6f2250fff237cb3f1cfb
Bivek Gurung <bivek.tamu@gmail.com>
2023-12-21 07:25:54 +1100

    Almost done

--- src/component/SortableList/SkillsBoard.jsx
+++ src/component/SortableList/SkillsBoard.jsx
@@ -5,20 +5,37 @@ import SuggestedSkills from './SuggestedSkills'
 
 const SkillsBoard = () => {
 
-    const [selectedSkills, setSS] = useState([])
-    const getSelectedSkills = (s) => {
-        setSS(s)
+    const [selectedSkill, setSelecedSkill] = useState()
+    const [lastDeselected, setLastDeselected] = useState()
+    const [suggestedSkill, setSuggestedSkill] = useState()
+
+    const updateSuggestedList = (s) => {
+        setSelecedSkill(s)
+        setLastDeselected('')
+
+    }
+    const getDeselectSkill = (s) => {
+        setLastDeselected(s)
+        setSelecedSkill('')
+        // addSkill('')
+        setSuggestedSkill('')
+
     }
 
-    const updateSkills = (s) => {
+    const addSkill = s=> {
         console.log('asdf')
+        setLastDeselected('')
+        setSuggestedSkill(s)
     }
+
+    // useEffect(()=> {console.log(lastDeselected)}, [lastDeselected])
+    
     return (
         <div className="Frame1 w-[717px] h-[498.85px] p-16 bg-white rounded-2xl shadow justify-start items-start gap-16 inline-flex">
 
-                <SearchBox skills={(s)=>getSelectedSkills(s)} />
-                <SuggestedSkills skills={selectedSkills} updateSkills={updateSkills()} />
-                
+            <SearchBox updateSuggestedList={(s) => updateSuggestedList(s)} lastDeselected={(s) => getDeselectSkill(s)} addSkill = {suggestedSkill} />
+            <SuggestedSkills lastSelected={selectedSkill} lastDeselected={lastDeselected} clickHandler={s=>addSkill(s)} />
+
         </div>
     )
 }
--- src/component/SortableList/SuggestedSkills.jsx
+++ src/component/SortableList/SuggestedSkills.jsx
@@ -1,36 +1,65 @@
 import React, { useEffect, useState } from 'react'
+import { SKILLS, SUGGESTED_SKILLS } from '../../dataLayer'
 
-const SuggestedSkills = ({skills}) => {
-    const INITIAL_SKILLS = ['HTML', 'CSS', 'Bootstrap', 'TypeScript', 'VueJs', 'Angular', 'NodeJs']
+
+const SuggestedSkills = ({ lastSelected, lastDeselected, clickHandler }) => {
+    const INITIAL_SKILLS = SUGGESTED_SKILLS
     const [suggestedSkills, setSS] = useState([])
-    const [clicked, setC] = useState([])
+    const [clicked, setClicked] = useState([])
+    const [updateSuggestedList, setUpdateSuggestedList] = useState()
     useEffect(() => {
         setSS(INITIAL_SKILLS)
-        const temp = INITIAL_SKILLS.map(s=> false)
-        setC(temp)
-
+        const temp = INITIAL_SKILLS.map(s => false)
+        setClicked(temp)
     }, [])
+
+    useEffect(() => {
+        lastSelected && setUpdateSuggestedList(lastSelected)
+    }, [lastSelected])
+
     const onClick = (s, i) => {
-        setC([...clicked.slice(0, i), true, ...clicked.slice(i+1)])
+        clickHandler(s)
+        setClicked([...clicked.slice(0, i), true, ...clicked.slice(i + 1)])
     }
-    useEffect(()=>{
-        if(clicked) {
-            console.log(clicked)
+    useEffect(() => {
+        if (clicked) {
+            // console.log(clicked)
         }
     }, [clicked])
 
-    
-    useEffect(()=>{
-        console.log(skills)
-        
+    useEffect(() => {
+        console.log(updateSuggestedList)
 
-    }, [skills])
+        if (updateSuggestedList) {
+            const index = INITIAL_SKILLS.indexOf(updateSuggestedList)
+            // console.log(index)
+            if (index > -1) {
+                setClicked([...clicked.slice(0, index), true, ...clicked.slice(index + 1)])
+            }
+
+        }
+
+    }, [updateSuggestedList])
+
+    useEffect(() => {
+        if (lastDeselected && lastDeselected !== '') {
+            console.log(lastDeselected)
+            const index = INITIAL_SKILLS.indexOf(lastDeselected)
+            if (index > -1) {
+                setClicked([...clicked.slice(0, index), false, ...clicked.slice(index + 1)])
+            }
+
+            setUpdateSuggestedList('')
+        }
+
+
+    }, [lastDeselected])
 
     return (
         <div className="SuggestedSkills w-[133px] h-[234px] relative">
             <div className="SuggestedSkills mb-4   text-blue-950 text-base font-semibold font-['Poppins']">Suggested Skills</div>
             <div className="Frame1  flex-col justify-start items-start gap-1.5">
-                {suggestedSkills.length > 0 && suggestedSkills.map((s, i) => <p key={i} onClick={()=>onClick(s, i)} className={`text-left text-slate-500 text-base font-normal font-['Poppins'] mb-1 rounded-lg  px-2 py-1 cursor-pointer ${clicked[i]&&'bg-blue-950 text-white'}`}>{clicked[i]===true?'-':'+'} {s}</p>)
+                {suggestedSkills.length > 0 && suggestedSkills.map((s, i) => <button key={i} onClick={() => onClick(s, i)} disabled={clicked[i]} className={`text-left w-full text-slate-500 text-base font-normal font-['Poppins'] mb-1 rounded-lg  px-2 py-1 cursor-pointer disabled ${clicked[i] && 'bg-blue-950 text-white'}`}>{clicked[i] === true ? '-' : '+'} {s}</button>)
                 }
             </div>
 
