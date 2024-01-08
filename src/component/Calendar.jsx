d4dd6f2fd84e5f2ed9584dca9c280c08b7732e8b
Bivek Gurung <bivek.tamu@gmail.com>
2024-01-08 22:59:41 +1100

    Calendar logic added, now remains UX

--- src/component/Calendar.jsx
+++ src/component/Calendar.jsx
@@ -1,55 +1,141 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 
 const Calendar = () => {
     const DATE = new Date()
     const SEVEN_DAYS = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun']
 
-    const [currDate, setCurrDate] = useState(DATE.getDate())
-    const [currMonth, setCurrMonth] = useState(DATE.getMonth())
-    const [currYear, setCurrYear] = useState(DATE.getFullYear())
-    const [totalDays, setTotalDays] = useState((new Date(DATE.getFullYear(), DATE.getMonth() + 1, 0)).getDate())
+    const [month, setMonth] = useState(DATE.getMonth())
+    const [year, setYear] = useState(DATE.getFullYear())
     const [calendar, setCalendar] = useState([])
+
+    // useEffect(() => {
+    //     // console.log(DATE.toLocaleString('default', {date: 'long'}))
+    //     setupCalendar()
+    //     pastMonth.current = month
+    //     console.log('b')
+    // }, [])
 
 
     useEffect(() => {
-        // console.log(DATE.toLocaleString('default', {date: 'long'}))
-        let tempCal = []
-        for (let i = 1; i <= totalDays; i++) {
-            const date = new Date(currYear, currMonth, i)
-
-            if (i === 1 && date.getDay() !== 0) {
-                for (let j = 1; j < date.getDay(); j++) {
-                    tempCal.push('')
-                }
-            }
-            tempCal.push(i)
-
+        if (month !== null) {
+            console.log(month)
+            setupCalendar()
         }
+    }, [month, year])
+
+    useEffect(()=> {
+        if(calendar.length > 0) {
+            // console.log(calendar)
+        }
+    }, [calendar])
+
+
+    function setupCalendar() {
+        let tempCal = [], totalDays = (new Date(year, month + 1, 0)).getDate(), day = (new Date(year, month, 1)).getDay()
+
+
+        day = (new Date(year, month, 1)).getDay()
+        if (day !== 1) {
+            const pastMonth = (new Date(year, month, 0)).getMonth()
+            let pastYear = year
+            if (pastMonth > 10) {
+                pastYear -= 1
+            }
+            // if(pastMonth === )
+            let pastMonthTotalDays = (new Date(year, month, 0)).getDate()
+            console.log(pastMonthTotalDays)
+            if (day === 0) {
+                day = 7
+            }
+            for (let i = pastMonthTotalDays - (day - 2); i <= pastMonthTotalDays; i++) {
+                tempCal.push({ y: pastYear, m: pastMonth, d: i })
+            }
+        }
+
+        for (let i = 1; i <= totalDays; i++) {
+            tempCal.push({ y: year, m: month, d: i })
+        }
+
+
+
+        day = (new Date(year, month, totalDays)).getDay()
+
+        if (day !== 0) {
+            const futureMonth = (month + 1)
+            let futureYear = year
+            for (let i = 1; i <= 7 - day; i++) {
+                if (tempCal.length > 41) {
+                    break
+                }
+
+                // tempCal.push(i)
+                tempCal.push({ y: futureYear, m: futureMonth, d: i })
+
+
+            }
+        }
+
+
         setCalendar([...tempCal])
-    }, [currMonth])
+    }
+
+
     const monthToString = (m) => {
         const months = ['January', 'February', 'March', 'April', 'May', 'June',
             'July', 'August', 'September', 'October', 'November', 'December']
         return months[m]
     }
+
+    const nextClickHandler = () => {
+        setMonth((prev) => {
+            
+            if(prev === 11) {
+                setYear((prev) => {
+                    return prev + 1
+                })
+                return 0
+            }
+            return prev + 1
+        })
+    }
+
+    const prevClickHandler = () => {
+        setMonth((prev) => {
+            if(prev === 0) {
+                setYear((prev) => {
+                    return prev - 1
+                })
+                return 11
+            }
+            return prev - 1
+        })
+    }
+
     return (
         <div className='calendar '>
-            <div className="flex items-center justify-between">\
-                <span>{monthToString(currMonth)} {currYear}</span>
-                <span onClick={()=>setCurrMonth((prev)=> {
-                    return prev+1
-                })}>Next</span>
+            <div className="flex items-center justify-between">
+                <span>{monthToString(month)} {year}</span>
+                <div className='flex justify-start gap-x-10'>
+                    <span onClick={() => prevClickHandler()}>Prev</span>
+                    <span onClick={() => nextClickHandler()}>Next</span>
+                </div>
             </div>
             <div className="grid grid-cols-7 gap-y-10">
-            {SEVEN_DAYS.length > 0 && SEVEN_DAYS.map((c, i) => {
+                {SEVEN_DAYS.length > 0 && SEVEN_DAYS.map((c, i) => {
                     return <span key={i}>{c}</span>
                 })}
                 {calendar.length > 0 && calendar.map((c, i) => {
                     let clas = ''
-                    if (currDate === c) {
-                        clas = 'bg-black text-white'
+
+                    if (DATE.getFullYear() === c.y && DATE.getMonth() === c.m && DATE.getDate() === c.d) {
+                        clas+="bg-black text-white"
                     }
-                    return <span className={`hover:bg-black hover:text-white ${currDate === c && 'bg-black text-white'}`} key={i}>{c}</span>
+                    else if(year !== c.y || month !== c.m) {
+                        clas+="text-red-500"
+                    }
+
+
+                    return <span className={`hover:bg-black hover:text-white ${clas && clas}`} key={i}>{c.d}</span>
                 })}
             </div>
         </div>
